94fa78745eace9be578d33345128d2850adcbb81
yeongo <yeongo@student.42seoul.kr>
2023-07-23 22:36:16 +0900

    remove main.cpp in ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,14 +2,5 @@
 
 int	main(void)
 {
-	ClapTrap a("A");
-	ClapTrap b("B");
-
-	a.attack("B");
-	b.takeDamage(5);
-	b.beRepaired(3);
-	b.attack("A");
-	a.takeDamage(10);
-	a.beRepaired(10);
 	return (0);
 }
